9eb2d51fe97506d96c9b98e1065dbae830f32268
Michael Nelson <michael.nelson@getfutureproof.co.uk>
2022-04-12 16:11:44 +0100

    Showing repo name, url & language

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 
 const RenderList = (props) =>{
     let values=props.data.map(d=><li>{d.id}</li>)
+    let values2=[]
+    let vals2=props.data.forEach(d => {
+        values2.push(
+        <li>
+            <a>{d.name}</a>
+            <a href={d.html_url}>{d.html_url}</a>
+            <a>{d.language}</a>
+        </li>)
+        
+    });
     return (
 			<div>
                 <h1>Results for:</h1>
-                <ul>{values}</ul>
+                <ul>{values2}</ul>
             </div>
 	);
 }
