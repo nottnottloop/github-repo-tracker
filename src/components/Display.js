457c52587f613e89b6d12c28a50a265357bc2a73
Michael Nelson <michael.nelson@getfutureproof.co.uk>
2022-04-13 07:53:39 +0100

    Added user doesnt exist error

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
 import './display.css'
 const RenderList = (props) =>{
-    //let values=props.data.map(d=><li>{d.id}</li>)
+
     let values2=[]
-    let vals2=props.data.forEach(d => {
-        values2.push(
-        <li>
-            <h2>{d.name}</h2>
-            <a target="_blank" href={d.html_url}>{d.html_url}</a><br></br>
-            <a>Languages: {d.language}</a><br></br>
-            <a>Last Push: {new Date(d.pushed_at).toLocaleDateString()} at {new Date(d.pushed_at).toLocaleTimeString()} </a><br></br>
-            <a>Stargazers: {d.stargazers_count}</a><br></br>
-            <a>Visibility: {d.visibility}</a><br></br>
-            <a>Forks: {d.forks}</a><br></br>
-            <a>Issues Count: {d.open_issues}</a>
-            
-        </li>)
+    let data=props.data
+    data.forEach(d => {
+            values2.push(
+            <li>
+              <h2>{d.name}</h2>
+                <a target="_blank" href={d.html_url}>{d.html_url}</a><br></br>
+                <a>Languages: {d.language}</a><br></br>
+                <a>Last Push: {new Date(d.pushed_at).toLocaleDateString()} at {new Date(d.pushed_at).toLocaleTimeString()} </a><br></br>
+                <a>Stargazers: {d.stargazers_count}</a><br></br>
+                <a>Visibility: {d.visibility}</a><br></br>
+                <a>Forks: {d.forks}</a><br></br>
+                <a>Issues Count: {d.open_issues}</a>
+                
+    </li>)
         
-    });
+})
+
+        
+    ;
     let user=props.data[0].owner.login
     return (
 			<div>
-                <h1>Results for {user} </h1>
+            
+                <div><h1>Results for {user} </h1>
                 <ul>{values2}</ul>
+                </div>
             </div>
 	);
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import RenderList from './Display';
 
 const Form = () =>{
-    const [inputVal, setInput] = useState({username: "",data:null});
+    const [inputVal, setInput] = useState({username: "",data:null,error:false});
 
     const onType = e => {
         console.log(e.target.value)
@@ -10,29 +10,38 @@ const Form = () =>{
         setInput(oldObj=>{ return {...oldObj, ...newObj}})
         
     }
-
+    let load={loading:true}
     const onSubmit = async e => {
         e.preventDefault()
         let username=inputVal.username
+        
+        console.log(inputVal)
         console.log(username)
         let options= {method: 'GET'}
         await fetch(`https://api.github.com/users/${username}/repos`,options)
          .then(res=>{
-             /*if (res.status==404){
-
-             }*/
+             if (res.status==404){
+              //GIVE USER DOESNT EXIST ERROR 
+              setInput(old=>{
+                  old.data=null;
+                  return old
+              })
+              return null
+             }
              return res.json()
         })
          .then(d=>{
-            const newObj={data: d}
+            const newObj={data: d,error:true}
             setInput(oldObj=>{ return {...oldObj, ...newObj}})
-            console.log(d,d.length)
         })
          
     }
        let searched=inputVal.data
+       let e=inputVal.error
+       console.log(e)
 		return (
             <div>
+                {e?<h1>User doesnt exist</h1>:null}
 			<form onSubmit={e=>onSubmit(e)}>
 				<input type="text" onChange={e=>onType(e)}></input>
                 <input type="submit" value="Search" />
